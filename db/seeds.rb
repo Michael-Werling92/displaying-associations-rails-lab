9da37f1139e85b89b00baf9b5a21df7e6763b87e
Michael <mfw6mw@virginia.edu>
2021-05-27 00:04:12 -0400

    commit tree 25e5a8981689070a7d2fee0c87d8888c8c813b07

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+drake = Artist.create!(name: "Coldplay")
+drake.songs.create!(title: "Yellow")
+drake.songs.create!(title: "Clocks")
+
+beyonce = Artist.create!(name: "Taylor Swift")
+beyonce.songs.create!(title: "Love Story")
